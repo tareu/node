098f83204ae9246c7684828ca3906bbb1daf94f3
tareu <cantilever10101010@gmail.com>
2025-06-11 12:54:37 +0530

    ...

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+function getRandomInt(min, max) {
+          const minCeiled = Math.ceil(min);
+          const maxFloored = Math.floor(max);
+          return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
 
 window.onload = (event) => {
-        document.getElementById("main").innerHTML = Math.random();
+        document.getElementById("main").innerHTML = getRandomInt(0,1);
 };
