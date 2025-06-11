f922b72e7ef6b3ffba151d70c6a4e47035acb288
tareu <cantilever10101010@gmail.com>
2025-06-11 13:42:46 +0530

    ...

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ window.onload = (event) => {
         var div = document.createElement("div");
         div.style.width = "1300px";
         div.style.height = "150px";
-        div.style.background = "grey";
-        div.style.color = "#5cfffc";
+        div.style.color = "grey";
+        div.style.background = "#5cfffc";
         div.style.position = "fixed";
         div.style.top = "50px"
         div.style.left = "50px"
