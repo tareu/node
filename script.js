fcb1db205691e6fa3374cbdc52fd2ee1d361dc03
tareu <cantilever10101010@gmail.com>
2025-06-11 13:23:58 +0530

    ...

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ window.onload = (event) => {
         div.style.height = "100px";
         div.style.background = "red";
         div.style.color = "white";
-        div.style.position = "relative";
+        div.style.position = "absolute";
         div.innerHTML = "Hello";
         document.getElementById("main").appendChild(div);
 
