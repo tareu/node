09c58c7e6c459b91fa29db6938af3cc5929a331e
tareu <cantilever10101010@gmail.com>
2025-06-11 13:22:04 +0530

    ...

--- script.js
+++ script.js
@@ -17,5 +17,12 @@ window.onload = (event) => {
         document.getElementById("main").innerHTML = text;
         document.getElementById("main").style.wordWrap = "break-word";
         document.getElementById("main").style.font = "italic bold 150px arial,serif";
+        var div = document.createElement("div");
+        div.style.width = "100px";
+        div.style.height = "100px";
+        div.style.background = "red";
+        div.style.color = "white";
+        div.innerHTML = "Hello";
+        document.getElementById("main").appendChild(div);
 
 };
