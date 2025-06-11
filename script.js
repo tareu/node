66b2d2c3caa57ce634353f4788e1dea35d917e89
tareu <cantilever10101010@gmail.com>
2025-06-11 13:08:54 +0530

    ...

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function getRandomInt(min, max) {
           const maxFloored = Math.floor(max);
           return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
-text = ""
-textlength = 10000
-for (let i = 0; i < textlength; i++) {
-        if (getRandomInt(0,2) == 0) {
-                text += "&#9608";
-        }
-        if (getRandomInt(0,2) == 1) {
-                text += "&#9617";
-        }
-}
 window.onload = (event) => {
+        text = ""
+        textlength = 10000
+        for (let i = 0; i < textlength; i++) {
+                if (getRandomInt(0,2) == 0) {
+                        text += "&#9608";
+                }
+                if (getRandomInt(0,2) == 1) {
+                        text += "&#9617";
+                }
+        }
         document.getElementById("main").innerHTML = text;
         document.getElementById("main").style.wordWrap = "break-word";
 
